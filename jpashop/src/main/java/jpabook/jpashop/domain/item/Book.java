50b86a666f9a609755058faeaae49a22bf7e6e86
sunwoong <sunwoong1122@gmail.com>
2023-06-10 23:32:00 +0900

    feat: 책 상품 @Builder & @NoArgsConstructor 적용

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,12 +1,22 @@
 package jpabook.jpashop.domain.item;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 public class Book extends Item {
     private String author;
     private String isbn;
+
+    @Builder
+    public Book(String author, String isbn) {
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
