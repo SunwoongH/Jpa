5d2588a1f99fc858068ee57762a7ed3bede1650f
sunwoong <sunwoong1122@gmail.com>
2023-06-15 20:43:53 +0900

    feat: 회원 목록 조회 기능 구현

--- jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -9,21 +9,23 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
+@RequestMapping("/members")
 @Controller
 public class MemberController {
     private final MemberService memberService;
 
-    @GetMapping("/members/new")
+    @GetMapping("new")
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/create-member-form";
     }
 
-    @PostMapping("/members/new")
+    @PostMapping("new")
     public String create(@Valid MemberForm memberForm, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "members/create-member-form";
@@ -32,11 +34,17 @@ public class MemberController {
                 .name(memberForm.getName())
                 .address(Address.builder()
                         .city(memberForm.getCity())
-                        .street(memberForm.getCity())
+                        .street(memberForm.getStreet())
                         .zipcode(memberForm.getZipcode())
                         .build())
                 .build();
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping()
+    public String list(Model model) {
+        model.addAttribute("members", memberService.findMembers());
+        return "members/member-list";
     }
 }
