e5555f151e598d7501da1db7b73c3f0d010b6844
sunwoong <sunwoong1122@gmail.com>
2023-06-18 15:50:24 +0900

    chore: 상품 비즈니스 로직 주석 추가

--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -17,21 +17,33 @@ import java.util.List;
 public class ItemService {
     private final ItemRepository itemRepository;
 
+    /**
+     * 상품 등록
+     */
     @Transactional
     public void saveItem(SaveItemDto saveItemDto) {
         itemRepository.save(saveItemDto.toBookEntity());
     }
 
+    /**
+     * 상품 수정
+     */
     @Transactional
     public void updateItem(Long id, UpdateItemDto updateItemDto) {
         Item item = itemRepository.findOne(id);
         item.update(updateItemDto.getName(), updateItemDto.getPrice(), updateItemDto.getStockQuantity());
     }
 
+    /**
+     * 상품 조회
+     */
     public ItemResponseDto findOne(Long id) {
         return ItemResponseDto.of(itemRepository.findOne(id));
     }
 
+    /**
+     * 상품 전체 조회
+     */
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
