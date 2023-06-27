ae1403d20de67b2a240a0057ccbadb3f433b10ba
sunwoong <sunwoong1122@gmail.com>
2023-06-27 23:56:55 +0900

    feat: 주문 컬렉션 조회 -> distinct fetch join로 최적화

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -89,4 +89,14 @@ public class OrderRepository {
                         " join fetch o.delivery d", Order.class
         ).getResultList();
     }
+
+    public List<Order> findAllWithItems() {
+        return entityManager.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class
+        ).getResultList();
+    }
 }
