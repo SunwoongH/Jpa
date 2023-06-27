f65226b5f468108db89e6bfdf8cdeee4723bfd4c
sunwoong <sunwoong1122@gmail.com>
2023-06-27 23:55:26 +0900

    feat: ResponseEntity & 주문 조회 기능 추가

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,9 +2,11 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.dto.ApiResponse;
 import jpabook.jpashop.dto.response.FindOrderResponseDto;
+import jpabook.jpashop.dto.response.FindSimpleOrderResponseDto;
 import jpabook.jpashop.repository.query.FindOrderQueryDto;
 import jpabook.jpashop.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +19,18 @@ import java.util.List;
 public class OrderApiController {
     private final OrderService orderService;
 
-    @GetMapping("/v1/orders")
-    public ApiResponse<List<FindOrderResponseDto>> findOrdersByFetchJoin() {
-        return ApiResponse.of(orderService.findOrdersByFetchJoin());
+    @GetMapping("/v1/simple/orders")
+    public ResponseEntity<ApiResponse<List<FindSimpleOrderResponseDto>>> findSimpleOrdersByFetchJoin() {
+        return ResponseEntity.ok(ApiResponse.of(orderService.findSimpleOrdersByFetchJoin()));
     }
 
-    @GetMapping("/v2/orders")
-    public ApiResponse<List<FindOrderQueryDto>> findOrdersByQueryDtos() {
-        return ApiResponse.of(orderService.findOrderQueryDtos());
+    @GetMapping("/v2/simple/orders")
+    public ResponseEntity<ApiResponse<List<FindOrderQueryDto>>> findSimpleOrdersByQueryDtos() {
+        return ResponseEntity.ok(ApiResponse.of(orderService.findOrderQueryDtos()));
+    }
+
+    @GetMapping("/v1/orders")
+    public ResponseEntity<ApiResponse<List<FindOrderResponseDto>>> findOrdersByFetchJoin() {
+        return ResponseEntity.ok(ApiResponse.of(orderService.findOrdersByFetchJoin()));
     }
 }
