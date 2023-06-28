8d2cec74e49d0586eb7aa44f24a52c82d5d4bbc6
sunwoong <sunwoong1122@gmail.com>
2023-06-28 20:50:39 +0900

    refactor: 회원 API -> ResponseEntity Wrapping 적용

--- jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,7 @@ import jpabook.jpashop.dto.response.JoinMemberResponseDto;
 import jpabook.jpashop.dto.response.UpdateMemberResponseDto;
 import jpabook.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,18 +21,18 @@ public class MemberApiController {
     private final MemberService memberService;
 
     @PostMapping
-    public ApiResponse<JoinMemberResponseDto> joinMember(@RequestBody @Valid final JoinMemberRequestDto joinMemberDto) {
-        return ApiResponse.of(memberService.join(joinMemberDto));
+    public ResponseEntity<ApiResponse<JoinMemberResponseDto>> joinMember(@RequestBody @Valid final JoinMemberRequestDto joinMemberDto) {
+        return ResponseEntity.ok(ApiResponse.of(memberService.join(joinMemberDto)));
     }
 
     @PutMapping("/{id}")
-    public ApiResponse<UpdateMemberResponseDto> updateMember(@PathVariable final Long id,
-                                                             @RequestBody @Valid final UpdateMemberRequestDto updateMemberDto) {
-        return ApiResponse.of(memberService.update(id, updateMemberDto));
+    public ResponseEntity<ApiResponse<UpdateMemberResponseDto>> updateMember(@PathVariable final Long id,
+                                                                             @RequestBody @Valid final UpdateMemberRequestDto updateMemberDto) {
+        return ResponseEntity.ok(ApiResponse.of(memberService.update(id, updateMemberDto)));
     }
 
     @GetMapping
-    public ApiResponse<List<FindMemberResponseDto>> findMembers() {
-        return ApiResponse.of(memberService.findMembers());
+    public ResponseEntity<ApiResponse<List<FindMemberResponseDto>>> findMembers() {
+        return ResponseEntity.ok(ApiResponse.of(memberService.findMembers()));
     }
 }
